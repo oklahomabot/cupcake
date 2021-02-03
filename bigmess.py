1ae85d129d918772e5f16fb5f2e46ecfd7b70357
oklahoma_bot <oklahoma_bot@mail.com>
2021-02-03 08:02:02 +0000

    One Cog added, web stuff started

---
+++ bigmess.py
@@ -0,0 +1,173 @@
+import discord
+from discord.ext import commands
+from replit import db
+import requests
+import json
+import random
+from web import start_web
+
+class AllOrders(commands.Cog):
+
+    def __init__(self, client):
+
+      self.client = client
+      if 'responding' in db.keys():
+        self.responding = db['responding']
+      else:
+        self.responding = False
+    '''
+    @commands.command()
+    async def on_command(ctx):
+      server = ctx.guild.name
+      user = ctx.author
+      command = ctx.command
+      await start_web(f'{server} \t {user} \t {command}')
+    '''
+    @commands.command()
+    async def inspire(self, ctx):
+      quote = get_quote()
+      await ctx.send(quote)
+
+    @commands.command()
+    async def new(self, ctx,* ,new_words):
+      update_encouragements(new_words)
+      await ctx.send("New encouraging message created")
+
+    @commands.command()
+    async def remove(self,ctx,index):
+      encouragements = []
+      temp_txt = ''
+      if 'encouragements' in db.keys():
+        encouragements = db['encouragements']
+        if encouragements:
+          try:
+            index = int(index)
+            delete_encouragements(index)
+            encouragements = db['encouragements']
+            temp_txt = (f'user created encouragements now include: {(" + ").join(encouragements)}')
+          except:
+            temp_txt = ('Something went wrong. Did you include an index number (starts at 0.)')
+        else:
+          temp_txt = ('Empty List : Add some using the "new" command')
+      else:
+        temp_txt = ('There is no list called "encouragements" in my memory')
+      
+      await ctx.send(temp_txt)
+
+    @commands.command()
+    async def sadwords(self,ctx):
+      await ctx.send(sad_words)
+
+    @commands.command()
+    async def list(self,ctx):
+      encouragements = []
+      temp_txt=('We do not have any user added encouraging messages saved')
+      if 'encouragements' in db.keys():
+        encouragements = db['encouragements']
+        if len(encouragements) != 0:
+          temp_txt = (f'user created encouragements include: {(" + ").join(encouragements)}')
+      await ctx.send(temp_txt)
+
+    @commands.command()
+    async def status(self,ctx):
+      if self.responding:
+        await ctx.send('I am listening')
+      else:
+        await ctx.send('I am distracted right now')
+
+    @commands.command()
+    async def responding(self,ctx, cmd):
+
+      if cmd.lower() in ['true','on']:
+        db['responding'] = True
+        self.responding = True
+        await ctx.send('RESPONDING is ON')
+      elif cmd.lower() in ['false','off']:
+        db['responding'] = False
+        self.responding = False
+        await ctx.send('RESPONDING is OFF')
+      else:
+        await ctx.send(f'STATUS unchanged, RESPONDING = {db["responding"]}')
+
+    @commands.command()
+    async def userinfo(self, ctx, user: discord.User = None):
+        temp = ''
+        if user is None:
+            await ctx.send('Please provide a user to get info')
+            return
+        temp = ((f'- User\'s ID: {user.id}\n') +
+                (f'- User\'s discrim: {user.discriminator}\n') +
+                ('- Is User a bot?: '))
+        temp = temp + ('YES' if user.bot else 'NO')
+
+        embed = discord.Embed(
+            title='Userinfo', description=f'misc. info about {user.name}', colour=discord.Colour.blue())
+
+        embed.add_field(name=user, value=temp)
+        if user.avatar is not None:
+            embed.set_thumbnail(url=user.avatar_url_as(size=64))
+
+        await ctx.send(':mag:', embed=embed)
+
+    @commands.command()
+    # passing ctx is context and is required
+    # it has such information as message ...
+    async def ping(self, ctx):
+        '''
+        I'll play
+        '''
+        # await is used with async to prevent blocking simple
+        # processes by allowing others to continue
+        await ctx.send(f'Pong: {round(self.client.latency*1000)}ms')
+
+
+    @commands.Cog.listener("on_message")
+    async def sad_check(self,message):
+      if message.author == self.client:
+        return
+  
+      if self.responding:
+        msg = message.content
+        msg_words = msg.split()
+        if any(word in msg_words for word in sad_words):
+          await message.channel.send(f'{random.choice(load_options())} {message.author.name}')
+
+# Helper Function
+def get_quote():
+  response = requests.get('https://zenquotes.io/api/random')
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + ' -' + json_data[0]['a']
+  return(quote)
+
+def update_encouragements(encouraging_message):
+  if "encouragements" in db.keys():
+    encouragements = db["encouragements"]
+    encouragements.append(encouraging_message)
+    db["encouragements"] = encouragements
+  else:
+    db['encouragements'] = [encouraging_message]
+
+def delete_encouragements(index):
+  encouragements = db['encouragements']
+  if len(encouragements) > index:
+    del encouragements[index]
+    db['encouragements'] = encouragements
+
+def setup(client):
+
+    client.add_cog(AllOrders(client))
+
+def load_options():
+  temp_list = starter_encouragements
+  if 'encouragements' in db.keys():
+    temp_list.extend(db['encouragements'])
+  return temp_list
+
+sad_words = ['sad', 'depressed', 'unhappy', 'miserable', 'depressing', 'cry', 'angry',
+             'upset', 'pissed', 'worried', 'scared', 'terrified']
+
+starter_encouragements = [
+  'Cheer up',
+  'Hang in there',
+  'You are a great person / bot'
+]
--- web.py
+++ web.py
@@ -1,17 +1,78 @@
-from flask import Flask
+import random, string
+from flask import Flask, render_template, request
 from threading import Thread
 import datetime
-
+'''
 app = Flask('')
 
 @app.route('/')
 def home():
     now = datetime.datetime.now()
     return (f'webserver accessed @ {now.strftime("%H:%M %m/%d/%Y")}')
+'''
+
+
+
+
+
+app = Flask(  # Create a flask app
+	__name__,
+	template_folder='templates',  # Name of html file folder
+	static_folder='static'  # Name of directory for static files
+)
+
+@app.route('/')  # What happens when the user visits the site
+def base_page(message = 'Just Started'):
+
+	random_num = random.randint(1, 100000)  # Sets the random number
+	return render_template(
+		'base.html',  # Template file path, starting from the templates folder. 
+		random_number=random_num,  # Sets the variable random_number in the template
+	  last_command = message)
+
 
 def run():
   app.run(host='0.0.0.0',port=8080)
 
-def persist():
+def start_web(message = 'Just Started'):
     t = Thread(target=run)
     t.start()
+
+if __name__ == "__main__":  # Makes sure this is the main process
+	app.run( # Starts the site
+		host='0.0.0.0',  # Establishes the host, required for repl to detect the site
+		port=8080)  # Randomly select the port the machine hosts on.
+
+
+
+
+
+'''
+import random, string
+from flask import Flask, render_template, request
+
+app = Flask(  # Create a flask app
+	__name__,
+	template_folder='templates',  # Name of html file folder
+	static_folder='static'  # Name of directory for static files
+)
+
+ok_chars = string.ascii_letters + string.digits
+
+
+
+
+
+@app.route('/2')
+def page_2():
+	rand_ammnt = random.randint(10, 100)
+	random_str = ''.join(random.choice(ok_chars) for a in range(rand_ammnt))
+	return render_template('site_2.html', random_str=random_str)
+
+
+if __name__ == "__main__":  # Makes sure this is the main process
+	app.run( # Starts the site
+		host='0.0.0.0',  # Establishes the host, required for repl to detect the site
+		port=random.randint(2000, 9000)  # Randomly select the port the machine hosts on.
+	)
+'''
