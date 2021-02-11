b111670dbf2a9757d61582065d73a6e1ed809681
oklahoma_bot <oklahoma_bot@mail.com>
2021-02-11 21:13:42 +0000

    secret

--- secret.py
+++ secret.py
@@ -1,22 +1,20 @@
 import discord
 from discord.ext import commands
-import json
-import requests
+
 
 class secret(commands.Cog):
   def __init__(self,client):
     self.client = client
 
   @commands.command(aliases=['Secret_Command','secret_command'], hidden=True)
-  async def secret(self,ctx):
-    def get_quote():
-      response = requests.get("https://zenquotes.io/api/random")
-      json_data = json.loads(response.text)
-      quote = json_data[0]['q'] + " -" + json_data[0]['a']
-      return(quote)
-    quote = get_quote()
-    embed=discord.Embed(title='Inspirational Quote',colour=discord.Colour.blue())
-    embed.add_field(name='Here is the quote...', value=quote,inline=False)
+  async def secret(self,ctx, message = None):
+        
+    description = 'Secret Message'
+    embed = discord.Embed(title='SECRET TITLE',colour=discord.Colour(0xE5E242),description=description)
+
+    embed.set_image(url="https://images.pexels.com/photos/3156660/pexels-photo-3156660.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500")
+    embed.set_thumbnail(url=ctx.message.author.avatar_url_as(size=64))
+
     await ctx.send(embed=embed)
 
 def setup(client):
